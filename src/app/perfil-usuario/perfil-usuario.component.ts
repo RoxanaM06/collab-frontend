c4a2bcb1032fc9b480c1361512785a3873373759
RoxanaM06 <roxymatur@gmail.com>
2023-08-25 15:06:20 -0600

    Mostrar información del usuario en la pantalla del perfil de usuario

--- src/app/perfil-usuario/perfil-usuario.component.ts
+++ src/app/perfil-usuario/perfil-usuario.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 import { NavbarService } from 'src/services/navbar.service';
+import { Usuario } from 'src/models/usuario.model';
 
 @Component({
   selector: 'app-perfil-usuario',
@@ -9,18 +10,42 @@ import { NavbarService } from 'src/services/navbar.service';
 })
 export class PerfilUsuarioComponent {
 
-  modalSwitchCambContra:boolean;
+  	modalSwitchCambContra:boolean;
+  	idUsuario:string;
+	usuarioActual:any;
 
 	constructor(private modalSS:ModalService,
 				private navbarS:NavbarService) { }
 
-	ngOnInit(): void {
+	async ngOnInit() {
 		this.modalSS.$modalCambContra.subscribe((valor)=>this.modalSwitchCambContra = valor);
 		this.navbarS.$opcionesProyecto.emit(false);
+		let usuario = await this.obtenerUsuario();
+		this.usuarioActual = usuario.usuarioObtenido;
+		console.log("Usuario en el ngOnInit", this.usuarioActual);
 	}
 
 	openModal() {
 		this.modalSwitchCambContra = true;
+	}
+
+	async obtenerUsuario() {
+
+		let usuario = window.localStorage.getItem("collab");
+
+		if(usuario!=null){
+			this.idUsuario = JSON.parse(usuario).idUsuario;
+		}
+		
+		let respuesta = await fetch(`http://localhost:3000/usuario/${this.idUsuario}`, {
+	    	method: "GET",
+	      	headers: {
+	       		"Content-type": "application/json"
+	     	}
+	    });
+	    let usuarioObtenido = await respuesta.json();
+
+		return usuarioObtenido;
 	}
 
 }
